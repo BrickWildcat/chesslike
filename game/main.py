36d86e91b06fa82aa27f3230087b874d3e97a899
Lucas Falker <luwifade@gmail.com>
2022-07-21 09:40:22 -0400

    Added knights

--- game/main.py
+++ game/main.py
@@ -3,7 +3,7 @@ import random as r
 import time
 hints = 0
 movetrtls = []
-
+spawn = 1
 
 screen=turtle.Screen()
 screen.title("Chesslike")
@@ -22,6 +22,7 @@ player.shape("game/WhitePawn.gif")
 # player.color("green")
 player.penup()
 player.goto(120, -120)
+playerpiece = "pawn"
 print("Spawned player")
 enemy = []
 
@@ -29,7 +30,13 @@ enemy = []
 def enemySpawn():
     global player, enemy
     newguy =turtle.Turtle()
-    newguy.shape("game/BlackPawn.gif")
+    if r.randint(0,2):
+        newguy.piece = "pawn"
+        newguy.shape("game/BlackPawn.gif")
+    else:
+        newguy.piece = "knight"
+        newguy.shape("square")
+        newguy.color("red")
     newguy.penup()
     newguy.goto(player.xcor(),player.ycor())
     while player.distance(newguy) == 0:
@@ -51,7 +58,8 @@ enemySpawn()
 print("Spawned enemy 2")
 
 
-mtpos = [[-80,0],[-80,80],[0,80],[80,80],[80,0],[80,-80],[0,-80],[-80,-80]]
+pawn = [[-80,0],[-80,80],[0,80],[80,80],[80,0],[80,-80],[0,-80],[-80,-80]]
+knight = [[-80,160],[80,160],[160,80],[160,-80],[80,-160],[-80,-160],[-160,80],[-160,-80]]
 for i in range(8):
     movetrtls.append(turtle.Turtle())
 for t in movetrtls:
@@ -61,23 +69,32 @@ for t in movetrtls:
     t.color("red")
     t.ht()
 
+def pieceArr(piece):
+    global pawn, knight
+    if piece == "pawn":
+        return pawn
+    elif piece == "knight":
+        return knight
 
-def enemyMove(emy):
+
+
+def enemyMove(emy,piece="pawn"):
     validmove = 0
+    piecearr = pieceArr(piece)
     while not validmove:
         weights = []
-        for xy in mtpos:
+        for xy in piecearr:
             x = emy.xcor() + xy[0]
             y = emy.ycor() + xy[1]
             weights.append((340-player.distance(x,y))*10)
-        seed = r.choices(mtpos,weights)
+        seed = r.choices(piecearr,weights)
         x = emy.xcor() + seed[0][0]
         y = emy.ycor() + seed[0][1]
         conflict = 0
         for e in enemy:
             if e.xcor() == x and e.ycor() == y:
                 conflict = 1
-        for xy in mtpos: 
+        for xy in piecearr: 
             cft = 0
             for e in enemy:
                 if e.xcor() == emy.xcor()+xy[0] and emy.ycor()+xy[1]:
@@ -95,15 +112,16 @@ def enemyMove(emy):
 
 
 def click(x, y):
-    global player, hints
+    global player, hints, spawn, playerpiece
     if player.distance(x,y) < 40:
         if not hints:
             hints = 1
+            piecearr = pieceArr(playerpiece)
             for i in range(8):
                 tx = player.xcor()
                 ty = player.ycor()
-                nx = mtpos[i][0]
-                ny = mtpos[i][1]
+                nx = piecearr[i][0]
+                ny = piecearr[i][1]
                 x = tx + nx
                 y = ty + ny
                 if x > 120 or y > 120 or x < -120 or y < -120:
@@ -118,19 +136,26 @@ def click(x, y):
     else:
         for t in movetrtls:
             if t.distance(x,y) < 40 and t.isvisible():
+                print("Moving player")
                 player.goto(t.xcor(),t.ycor())
                 enemyKill()
                 hints = 0
                 for t in movetrtls:
                     t.ht()
                 screen.update()
+                print("Moved player")
                 o=0
                 for en in enemy:
                     o+=1
                     print("Moving enemy",o)
-                    enemyMove(en)
+                    enemyMove(en,en.piece)
                     print("Moved enemy", o)
-                break
+                if spawn == 5 or len(enemy) == 0:
+                    time.sleep(0.5)
+                    enemySpawn()
+                    spawn = 0
+                else:
+                    spawn += 1
 
 
 
